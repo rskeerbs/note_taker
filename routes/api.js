4f9936877372c6d3f513b2f481a9bcbb776e28a5
Romi Keerbs <rskeerbs@gmail.com>
2020-05-02 04:19:00 -0700

    Made some changes

--- routes/api.js
+++ routes/api.js
@@ -1,14 +1,20 @@
 app.get('/api/notes', function (req, res) {
-    fs.readFile(db/db.json),  
-    return res.json(getNotes)
+    fs.readFile(db.json, renderNoteList, (error) => {
+      console.log(error);
+    }  
+    return res.json(db.json, getNotes)
   })
 
   app.post("/api/notes", function(req, res) {
-    fs.writeFile(db/db.json),
-    return res.json(saveNote);
+    fs.appendFile(db.json, handleNoteSave, (error) => {
+      console.log(error);
+    }
+    return res.json(db.json, saveNote);
   });
 
   app.delete("/api/notes/:id", function(req, res) {
-    fs.writeFile(db/db.json),
-    return res.json(deleteNote);
+    fs.appendFile(db.json, handleNoteDelete, (error) => {
+      console.log(error);
+    }
+    return res.json(db.json, deleteNote);
   });
